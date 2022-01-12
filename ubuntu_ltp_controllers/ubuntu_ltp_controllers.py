c5546d04bc7bc89091b1830243da2a0469487269
Po-Hsu Lin <po-hsu.lin@canonical.com>
2022-01-12 16:07:42 +0800

    UBUNTU: SAUCE: ubuntu_ltp_controllers: bump timeout for memcg_stress test
    BugLink: https://bugs.launchpad.net/bugs/1946348
    
    The default 35 minutes timeout defined in memcg_stress_test.sh from
    ltp repo seems not enough for this test to run on some instances, use
    LTP_TIMEOUT_MUL=2 to double it to 70 minutes.
    
    We will need to bump the overall timeout setting for the autotest
    framework as well. Otherwise even with timeout doubled to 70 mins, it
    will still gets killed because the timeout for sub-tests was set to
    60 minutes.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_ltp_controllers/ubuntu_ltp_controllers.py
+++ ubuntu_ltp_controllers/ubuntu_ltp_controllers.py
@@ -120,6 +120,9 @@ class ubuntu_ltp_controllers(test.test):
             if utils.system_output('virt-what', verbose=False):
                 print("Running in VM, set timeout multiplier LTP_TIMEOUT_MUL=12 for memcg_test_3 (lp:1836694)")
                 os.environ["LTP_TIMEOUT_MUL"] = '12'
+        elif test_name == 'memcg_stress':
+            print("Set timeout multiplier LTP_TIMEOUT_MUL=2 for memcg_stress (lp:1946348)")
+            os.environ["LTP_TIMEOUT_MUL"] = '2'
 
         cmd = '/opt/ltp/runltp -f /tmp/target -q -C /dev/null -l /dev/null -T /dev/null'
         print(utils.system_output(cmd, verbose=False))

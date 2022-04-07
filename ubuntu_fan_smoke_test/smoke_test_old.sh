Cypresslin/autotest-client-tests

commit 56a28479bc31706cb6fb83509dacb53a284244be

UBUNTU: SAUCE: ubuntu_fan_smoke_test: fix arm64 docker pull command
BugLink: https://bugs.launchpad.net/bugs/1967990

Docker pull on arm64 will fail with:
    Error response from daemon: manifest for aarch64/ubuntu:latest
    not found: manifest unknown: manifest unknown

Change the way how we pull it with --platform flag.

Corresponding tags can be found here:
  https://registry.hub.docker.com/_/ubuntu?tab=tags

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_fan_smoke_test/smoke_test_old.sh
@@ -188,13 +188,15 @@ if [ "$UNDERLAY" = "" ]; then
 	exit 1
 fi
 
-image="`uname -m`/ubuntu"
-[ "`uname -m`" = "x86_64" ] && image="ubuntu"
-# i386 is an official repository on docker.io, i686 is not
-[ "`uname -m`" = "i686" ] && image="i386/ubuntu"
-echo -n "docker pull $image: "
+arch="`uname -m`"
+[ "`uname -m`" = "x86_64" ] && arch='amd64'
+[ "`uname -m`" = "i686" ] && arch='386'
+[ "`uname -m`" = "aarch64" ] && arch='arm64'
+image='ubuntu'
+opt="--platform linux/$arch"
+echo -n "docker pull $opt $image: "
 
-docker pull $image > /dev/null
+docker pull $opt $image > /dev/null
 ret=$?
 if [ $ret -ne 0 ]; then
 	echo "FAILED (docker pull returned $ret)"

commit c6c5feef7771649c263d4d10e90331a186ba385c

UBUNTU: SAUCE: ubuntu_fan_smoke_test: fix --platform flag for Xenial
BugLink: https://bugs.launchpad.net/bugs/1968030

The --platform flag is only available in experimental mode of the
docker daemon on Xenial. Let's not use this flag on Xenial.

Fixes: 56a28479bc31 ("UBUNTU: SAUCE: ubuntu_fan_smoke_test: fix arm64 docker pull command")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_fan_smoke_test/smoke_test_old.sh
@@ -193,7 +193,10 @@ arch="`uname -m`"
 [ "`uname -m`" = "i686" ] && arch='386'
 [ "`uname -m`" = "aarch64" ] && arch='arm64'
 image='ubuntu'
+series=`lsb_release -c | awk '{print $2}'`
 opt="--platform linux/$arch"
+# Xenial only support --platform flag in experimental mode
+[ "$series" = "xenial" ] && opt=""
 echo -n "docker pull $opt $image: "
 
 docker pull $opt $image > /dev/null
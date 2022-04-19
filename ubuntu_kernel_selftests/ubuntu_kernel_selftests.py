0aa1ba5b9128e8eb03afd814213cf86c8867a4ae
Po-Hsu Lin <po-hsu.lin@canonical.com>
2022-04-19 15:53:50 +0800

    UBNUTU: SAUCE: ubuntu_kernel_selftests: install modules-extra for oracle
    BugLink: https://bugs.launchpad.net/bugs/1969431
    
    Without the modules-extra package, these following tests will fail on
    with oracle kernel:
      * rtnetlink.sh
      * net:fib_tests.sh
      * fib-onlink-tests.sh
      * test_vxlan_under_vrf.sh
      * l2tp.sh
      * fib_nexthops.sh
      * icmp.sh
      * icmp_redirect.sh
      * vrf-xfrm-tests.sh
    
    Just like failures in ubuntu_quota_smoke_test / ubuntu_zram_smoke_test,
    this issue begins since F-oracle 5.13.0-1023.28~20.04.1
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
+++ ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -33,7 +33,7 @@ class ubuntu_kernel_selftests(test.test):
         gcc = 'gcc' if self.arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
         pkgs.append(gcc)
 
-        if any(x in self.flavour for x in ['aws', 'azure', 'gcp', 'gke']):
+        if any(x in self.flavour for x in ['aws', 'azure', 'gcp', 'gke', 'oracle']):
             if not (self.flavour == 'aws' and self.series == 'trusty'):
                 pkgs.append('linux-modules-extra-' + platform.uname()[2])
 

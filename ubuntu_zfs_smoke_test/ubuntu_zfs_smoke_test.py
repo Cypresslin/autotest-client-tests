Cypresslin/autotest-client-tests

commit 4c9ac393e457971106b8d55b5437ee58210f2f0b

UBUNTU: SAUCE: Remove zfs-related packages after test
BugLink: https://bugs.launchpad.net/bugs/1892124

Stop zfs daemon and remove zfs related packages after test to prevent
manually provisioned system from booting with out-of-tree zfs modules
and failed with the kernel_tainted test in ubuntu_boot like:
    Kernel taint value is 4097
    Taint bit value: 0 (proprietary module was loaded)
    *   Modules with GPL Incompatible Licenses:
         zfs: CDDL
         zunicode: CDDL
         zcommon: CDDL
         znvpair: CDDL
         zavl: CDDL
         icp: CDDL
    Taint bit value: 12 (externally-built ('out-of-tree') module was loaded)
    *   Modules not in-tree:
         zfs
         zunicode
         zzstd
         zlua
         zcommon
         znvpair
         zavl
         icp
         spl

This is done by adding a sub-test called post-test-zfs-cleanup and
catch it in run_once(). Not using cleanup() because it will be
triggered after each sub test. It's fine for test like
ubuntu_zfs_stress, but it will be complicated for test with multiple
sub-tests e.g. ubuntu_zfs_xfs_generic

The downside of this is that if you're trying to run it again on
the same SUT manully without removing the autotest/client/tmp/
directory, the setup task won't be triggered as the test version
didn't change. To workaround this we will need to move the code in
setup() to initialize() like what we did for ubuntu_sysdig_smoke_test,
to trigger the package installation, or, remove the .version file in
autotest/client/tmp/TESTNAME/

Search criteria to locate tests that need this change:
    grep -r zfsutils-linux *

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
Acked-by: Sean Feole <sean.feole@canonical.com>
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_performance_fio/ubuntu_performance_fio.py
@@ -408,6 +408,15 @@ class ubuntu_performance_fio(test.test):
             self.setup()
             self.get_sysinfo()
             return
+        elif test_name == 'post-test-zfs-cleanup':
+            utils.system('systemctl stop zed')
+            utils.system('modprobe -r zfs')
+            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+            # Remove .version for the test, in order to trigger setup() again if we want re-test it
+            cmd = 'rm {}/.version'.format(self.srcdir)
+            utils.system(cmd)
+            return
 
         #
         #  Drop cache to get a good idea of how much free memory can be used

ubuntu_performance_zfs_encryption/ubuntu_performance_zfs_encryption.py
@@ -38,6 +38,16 @@ class ubuntu_performance_zfs_encryption(test.test):
     def run_once(self, test_name):
         if test_name == 'setup':
             return
+        elif test_name == 'post-test-zfs-cleanup':
+            utils.system('systemctl stop zed')
+            utils.system('modprobe -r zfs')
+            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+            # Remove .version for the test, in order to trigger setup() again if we want re-test it
+            cmd = 'rm {}/.version'.format(self.srcdir)
+            utils.system(cmd)
+            return
+
         cmd = '%s/%s %s' % (self.bindir, test_name, self.srcdir)
         self.results = utils.system_output(cmd, retain_output=True)
         print(self.results)

ubuntu_zfs/ubuntu_zfs.py
@@ -77,6 +77,15 @@ class ubuntu_zfs(test.test):
     def run_once(self, test_name):
         if test_name == 'setup':
             return
+        elif test_name == 'post-test-zfs-cleanup':
+            utils.system('systemctl stop zed')
+            utils.system('modprobe -r zfs')
+            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+            # Remove .version for the test, in order to trigger setup() again if we want re-test it
+            cmd = 'rm {}/.version'.format(self.srcdir)
+            utils.system(cmd)
+            return
 
         os.chdir(self.srcdir)
         cmd = 'RUNFILE="-c %s/linux.run" make test' % self.srcdir

ubuntu_zfs_fstest/ubuntu_zfs_fstest.py
@@ -64,7 +64,16 @@ class ubuntu_zfs_fstest(test.test):
         #  invoked, then we can run run_once per test
         #
         if test_name == 'setup':
-                return
+            return
+        elif test_name == 'post-test-zfs-cleanup':
+            utils.system('systemctl stop zed')
+            utils.system('modprobe -r zfs')
+            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+            # Remove .version for the test, in order to trigger setup() again if we want re-test it
+            cmd = 'rm {}/.version'.format(self.srcdir)
+            utils.system(cmd)
+            return
 
         os.chdir(self.srcdir)
         tmp_pool = os.path.join(self.srcdir, 'pool.img')

ubuntu_zfs_stress/ubuntu_zfs_stress.py
@@ -63,6 +63,16 @@ class ubuntu_zfs_stress(test.test):
         utils.system('modprobe zfs')
 
     def run_once(self, test_name):
+        if test_name == 'post-test-zfs-cleanup':
+            utils.system('systemctl stop zed')
+            utils.system('modprobe -r zfs')
+            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+            # Remove .version for the test, in order to trigger setup() again if we want re-test it
+            cmd = 'rm {}/.version'.format(self.srcdir)
+            utils.system(cmd)
+            return
+
         stress_ng = os.path.join(self.srcdir, 'stress-ng', 'stress-ng')
         #
         #  temp logfile

ubuntu_zfs_xfs_generic/ubuntu_zfs_xfs_generic.py
@@ -131,7 +131,17 @@ class ubuntu_zfs_xfs_generic(test.test):
         #  invoked, then we can run run_once per test
         #
         if test_name == 'setup':
-                return
+            return
+        elif test_name == 'post-test-zfs-cleanup':
+            utils.system('systemctl stop zed')
+            utils.system('modprobe -r zfs')
+            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+            # Remove .version for the test, in order to trigger setup() again if we want re-test it
+            cmd = 'rm {}/.version'.format(self.srcdir)
+            utils.system(cmd)
+            return
+
         os.chdir(os.path.join(self.srcdir, 'xfstests-bld', 'xfstests-dev'))
         cmd = '%s/ubuntu_zfs_xfs_generic.sh %s %s' % (self.bindir, test_name, self.srcdir)
         print("Running: " + cmd)

ubuntu_zfs_smoke_test/ubuntu_zfs_smoke_test.py
@@ -38,6 +38,16 @@ class ubuntu_zfs_smoke_test(test.test):
     def run_once(self, test_name):
         if test_name == 'setup':
             return
+        elif test_name == 'post-test-zfs-cleanup':
+            utils.system('systemctl stop zed')
+            utils.system('modprobe -r zfs')
+            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+            # Remove .version for the test, in order to trigger setup() again if we want re-test it
+            cmd = 'rm {}/.version'.format(self.srcdir)
+            utils.system(cmd)
+            return
+
         cmd = '%s/%s %s' % (self.bindir, test_name, self.srcdir)
         self.results = utils.system_output(cmd, retain_output=True)
 

ubuntu_performance_fio/control
@@ -27,6 +27,7 @@ tests = [
 
     'rd-75,wr-25,rand,blk-128k,jobs-16',
     'rd-75,wr-25,rand,blk-8k,jobs-64',
+    'post-test-zfs-cleanup',
 ]
 
 results = job.run_test_detail('ubuntu_performance_fio', test_name='setup', tag='setup', media='', timeout=60*30)

commit 42d98c929a74dcfc5c998dc34a67f2f0a52683d0

UBUNTU: SAUCE: Don't cleanup ZFS on system with ZFS filesystem in use
BugLink: https://bugs.launchpad.net/bugs/1970600/

The post-test-zfs-cleanup add for zfs-related tests will break system
that runs on ZFS. Check if the SUT is using it with "df -t zfs" before
trying to do the cleanup.

Patch tested on s390x LPAR with ubuntu_zfs_smoke_test test (not a
zfs-based system though).

Fixes: 4c9ac393e457 ("UBUNTU: SAUCE: Remove zfs-related packages after test")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
Acked-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_performance_fio/ubuntu_performance_fio.py
@@ -7,7 +7,7 @@ import re
 import subprocess
 import resource
 import shutil
-from autotest.client import test, utils
+from autotest.client        import test, utils
 from autotest.client.shared import error
 
 TEST_FILESYSTEM = os.getenv('TEST_FILESYSTEM')
@@ -409,13 +409,19 @@ class ubuntu_performance_fio(test.test):
             self.get_sysinfo()
             return
         elif test_name == 'post-test-zfs-cleanup':
-            utils.system('systemctl stop zed')
-            utils.system('modprobe -r zfs')
-            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
-            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
-            # Remove .version for the test, in order to trigger setup() again if we want re-test it
-            cmd = 'rm {}/.version'.format(self.srcdir)
-            utils.system(cmd)
+            # Make sure there is no ZFS in use by any filesystem
+            try:
+                utils.system('df -t zfs &> /dev/null') # return 1 if not found
+                print("ZFS in use by filesystem, SKIP cleanup")
+            except error.CmdError:
+                print("Stop / unload / remove ZFS")
+                utils.system('systemctl stop zed')
+                utils.system('modprobe -r zfs')
+                # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+                utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+                # Remove .version for the test, in order to trigger setup() again if we want re-test it
+                cmd = 'rm {}/.version'.format(self.srcdir)
+                utils.system(cmd)
             return
 
         #

ubuntu_performance_zfs_encryption/ubuntu_performance_zfs_encryption.py
@@ -1,6 +1,7 @@
 #
 #
 from autotest.client                        import test, utils
+from autotest.client.shared                 import error
 import platform
 
 class ubuntu_performance_zfs_encryption(test.test):
@@ -39,13 +40,19 @@ class ubuntu_performance_zfs_encryption(test.test):
         if test_name == 'setup':
             return
         elif test_name == 'post-test-zfs-cleanup':
-            utils.system('systemctl stop zed')
-            utils.system('modprobe -r zfs')
-            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
-            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
-            # Remove .version for the test, in order to trigger setup() again if we want re-test it
-            cmd = 'rm {}/.version'.format(self.srcdir)
-            utils.system(cmd)
+            # Make sure there is no ZFS in use by any filesystem
+            try:
+                utils.system('df -t zfs &> /dev/null') # return 1 if not found
+                print("ZFS in use by filesystem, SKIP cleanup")
+            except error.CmdError:
+                print("Stop / unload / remove ZFS")
+                utils.system('systemctl stop zed')
+                utils.system('modprobe -r zfs')
+                # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+                utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+                # Remove .version for the test, in order to trigger setup() again if we want re-test it
+                cmd = 'rm {}/.version'.format(self.srcdir)
+                utils.system(cmd)
             return
 
         cmd = '%s/%s %s' % (self.bindir, test_name, self.srcdir)

ubuntu_zfs/ubuntu_zfs.py
@@ -78,13 +78,19 @@ class ubuntu_zfs(test.test):
         if test_name == 'setup':
             return
         elif test_name == 'post-test-zfs-cleanup':
-            utils.system('systemctl stop zed')
-            utils.system('modprobe -r zfs')
-            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
-            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
-            # Remove .version for the test, in order to trigger setup() again if we want re-test it
-            cmd = 'rm {}/.version'.format(self.srcdir)
-            utils.system(cmd)
+            # Make sure there is no ZFS in use by any filesystem
+            try:
+                utils.system('df -t zfs &> /dev/null') # return 1 if not found
+                print("ZFS in use by filesystem, SKIP cleanup")
+            except error.CmdError:
+                print("Stop / unload / remove ZFS")
+                utils.system('systemctl stop zed')
+                utils.system('modprobe -r zfs')
+                # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+                utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+                # Remove .version for the test, in order to trigger setup() again if we want re-test it
+                cmd = 'rm {}/.version'.format(self.srcdir)
+                utils.system(cmd)
             return
 
         os.chdir(self.srcdir)

ubuntu_zfs_fstest/ubuntu_zfs_fstest.py
@@ -4,6 +4,7 @@ import multiprocessing
 import os
 import platform
 from autotest.client                        import test, utils
+from autotest.client.shared                 import error
 
 class ubuntu_zfs_fstest(test.test):
     version = 1
@@ -66,13 +67,19 @@ class ubuntu_zfs_fstest(test.test):
         if test_name == 'setup':
             return
         elif test_name == 'post-test-zfs-cleanup':
-            utils.system('systemctl stop zed')
-            utils.system('modprobe -r zfs')
-            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
-            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
-            # Remove .version for the test, in order to trigger setup() again if we want re-test it
-            cmd = 'rm {}/.version'.format(self.srcdir)
-            utils.system(cmd)
+            # Make sure there is no ZFS in use by any filesystem
+            try:
+                utils.system('df -t zfs &> /dev/null') # return 1 if not found
+                print("ZFS in use by filesystem, SKIP cleanup")
+            except error.CmdError:
+                print("Stop / unload / remove ZFS")
+                utils.system('systemctl stop zed')
+                utils.system('modprobe -r zfs')
+                # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+                utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+                # Remove .version for the test, in order to trigger setup() again if we want re-test it
+                cmd = 'rm {}/.version'.format(self.srcdir)
+                utils.system(cmd)
             return
 
         os.chdir(self.srcdir)

ubuntu_zfs_stress/ubuntu_zfs_stress.py
@@ -5,6 +5,7 @@ import os
 import platform
 import shutil
 from autotest.client                        import test, utils
+from autotest.client.shared                 import error
 
 class ubuntu_zfs_stress(test.test):
     version = 1
@@ -64,13 +65,19 @@ class ubuntu_zfs_stress(test.test):
 
     def run_once(self, test_name):
         if test_name == 'post-test-zfs-cleanup':
-            utils.system('systemctl stop zed')
-            utils.system('modprobe -r zfs')
-            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
-            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
-            # Remove .version for the test, in order to trigger setup() again if we want re-test it
-            cmd = 'rm {}/.version'.format(self.srcdir)
-            utils.system(cmd)
+            # Make sure there is no ZFS in use by any filesystem
+            try:
+                utils.system('df -t zfs &> /dev/null') # return 1 if not found
+                print("ZFS in use by filesystem, SKIP cleanup")
+            except error.CmdError:
+                print("Stop / unload / remove ZFS")
+                utils.system('systemctl stop zed')
+                utils.system('modprobe -r zfs')
+                # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+                utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+                # Remove .version for the test, in order to trigger setup() again if we want re-test it
+                cmd = 'rm {}/.version'.format(self.srcdir)
+                utils.system(cmd)
             return
 
         stress_ng = os.path.join(self.srcdir, 'stress-ng', 'stress-ng')

ubuntu_zfs_xfs_generic/ubuntu_zfs_xfs_generic.py
@@ -5,6 +5,7 @@ import platform
 import shutil
 from autotest.client                        import test, utils
 from autotest.client                        import canonical
+from autotest.client.shared                 import error
 
 class ubuntu_zfs_xfs_generic(test.test):
     version = 5
@@ -133,13 +134,19 @@ class ubuntu_zfs_xfs_generic(test.test):
         if test_name == 'setup':
             return
         elif test_name == 'post-test-zfs-cleanup':
-            utils.system('systemctl stop zed')
-            utils.system('modprobe -r zfs')
-            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
-            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
-            # Remove .version for the test, in order to trigger setup() again if we want re-test it
-            cmd = 'rm {}/.version'.format(self.srcdir)
-            utils.system(cmd)
+            # Make sure there is no ZFS in use by any filesystem
+            try:
+                utils.system('df -t zfs &> /dev/null') # return 1 if not found
+                print("ZFS in use by filesystem, SKIP cleanup")
+            except error.CmdError:
+                print("Stop / unload / remove ZFS")
+                utils.system('systemctl stop zed')
+                utils.system('modprobe -r zfs')
+                # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+                utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+                # Remove .version for the test, in order to trigger setup() again if we want re-test it
+                cmd = 'rm {}/.version'.format(self.srcdir)
+                utils.system(cmd)
             return
 
         os.chdir(os.path.join(self.srcdir, 'xfstests-bld', 'xfstests-dev'))

ubuntu_zfs_smoke_test/ubuntu_zfs_smoke_test.py
@@ -1,6 +1,7 @@
 #
 #
 from autotest.client                        import test, utils
+from autotest.client.shared                 import error
 import platform
 
 class ubuntu_zfs_smoke_test(test.test):
@@ -39,13 +40,19 @@ class ubuntu_zfs_smoke_test(test.test):
         if test_name == 'setup':
             return
         elif test_name == 'post-test-zfs-cleanup':
-            utils.system('systemctl stop zed')
-            utils.system('modprobe -r zfs')
-            # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
-            utils.system('apt-get remove --yes --force-yes zfsutils-linux')
-            # Remove .version for the test, in order to trigger setup() again if we want re-test it
-            cmd = 'rm {}/.version'.format(self.srcdir)
-            utils.system(cmd)
+            # Make sure there is no ZFS in use by any filesystem
+            try:
+                utils.system('df -t zfs &> /dev/null') # return 1 if not found
+                print("ZFS in use by filesystem, SKIP cleanup")
+            except error.CmdError:
+                print("Stop / unload / remove ZFS")
+                utils.system('systemctl stop zed')
+                utils.system('modprobe -r zfs')
+                # No need to consider ubuntu-zfs package on P/T as they've been blacklisted
+                utils.system('apt-get remove --yes --force-yes zfsutils-linux')
+                # Remove .version for the test, in order to trigger setup() again if we want re-test it
+                cmd = 'rm {}/.version'.format(self.srcdir)
+                utils.system(cmd)
             return
 
         cmd = '%s/%s %s' % (self.bindir, test_name, self.srcdir)
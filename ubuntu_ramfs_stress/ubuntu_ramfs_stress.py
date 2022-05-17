2513e0e9c129903377a972a30b8af201fef465a7
Po-Hsu Lin <po-hsu.lin@canonical.com>
2022-05-17 12:18:15 +0800

    UBUNTU: SAUCE: ubuntu_ramfs_stress: fix test build (cannot find -lapparmor)
    BugLink: https://bugs.launchpad.net/bugs/1973673
    
    With stress-ng fork updated to 8a8add42 (V0.14.01+)
    
    The test build will fail with:
      CC stress-io-uring.c
      CC stress-personality.c
      LD stress-ng
      Makefile:439: recipe for target 'stress-ng' failed
      make[1]: Leaving directory '/home/ubuntu/autotest/client/tmp/ubuntu_ramfs_stress/src/stress-ng'
      Makefile:416: recipe for target 'all' failed
      stderr:
      /usr/bin/ld: cannot find -lapparmor
      collect2: error: ld returned 1 exit status
      make[1]: *** [stress-ng] Error 1
    
    It needs the libapparmor-dev package.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_ramfs_stress/ubuntu_ramfs_stress.py
+++ ubuntu_ramfs_stress/ubuntu_ramfs_stress.py
@@ -30,6 +30,7 @@ class ubuntu_ramfs_stress(test.test):
             'pax',
             'xfsprogs',
             'libattr1-dev',
+            'libapparmor-dev',
             'libkeyutils-dev',
         ]
         gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'

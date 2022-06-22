Cypresslin/autotest-client-tests

commit 6d4527fd606133066527a2617dfd73c5bfd98b2d

UBUNTU: SAUCE: ubuntu_bpf: fix pahole-flags.sh permission denied issue
BugLink: https://bugs.launchpad.net/bugs/1979305

The pahole-flags.sh script is not executable when extracted from apt
source, we must run debian/rules clean to correct it like what we do
for ubuntu_kernel_selftests. Otherwise the test build will complain:

  sudo make TARGETS=bpf SKIP_TARGETS= clean all
  make[1]: Entering directory '/home/ubuntu/autotest/client/tmp/ubuntu_bpf/src/linux/tools/testing/selftests/bpf'
    CLEAN
  /bin/sh: 1: ./scripts/pahole-flags.sh: Permission denied
  /bin/sh: 1: ./scripts/pahole-flags.sh: Permission denied
    CLEAN   eBPF_helpers-manpage
    CLEAN   eBPF_syscall-manpage

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_bpf/ubuntu_bpf.py
@@ -65,6 +65,13 @@ class ubuntu_bpf(test.test):
             self.download()
         # Assist local testing by restoring the linux repo to vanilla.
         self.extract()
+
+        # clean source tree so changes from debian.foo/reconstruct
+        # (e.g. deleting files) are applied
+        os.chdir('linux')
+        cmd = 'fakeroot debian/rules clean'
+        utils.system(cmd)
+
         os.chdir(self.srcdir)
 
         #

commit c73a72ae9287200138a0bc94efe903a3c5e804a4

UBUNTU: SAUCE: ubuntu_bpf: add debhelper package
The debian/rule clean execution would require the debhelper package,
otherwise it will complain:
  /bin/bash: line 1: dh_testdir: command not found

Fixes: 6d4527fd6061 ("UBUNTU: SAUCE: ubuntu_bpf: fix pahole-flags.sh permission denied issue")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_bpf/ubuntu_bpf.py
@@ -12,6 +12,7 @@ class ubuntu_bpf(test.test):
 
         pkgs = [
             'build-essential',
+            'debhelper',
             'docutils-common',
             'git',
             'libcap-dev',
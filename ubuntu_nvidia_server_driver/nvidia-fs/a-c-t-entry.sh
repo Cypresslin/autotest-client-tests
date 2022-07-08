60f52f9039f88f4acd659e267b3c572a52875cfa
Taihsiang Ho (tai271828) <taihsiang.ho@canonical.com>
2022-07-08 12:31:19 +0200

    UBUNTU: SAUCE: ubuntu_nvidia_server_driver: nvidia-fs: fail the job correctly
    This commit fixes the false positive of a-c-t-entry.sh, which is a
    wrapper to make sure a-c-t invoke our target script in the right
    directory context. The wrapper itself should fail correspondingly if the
    target script fails, otherwise, there will be false positive of a-c-t
    job.
    
    Appending set -x for us to have debugging information in the future by
    default, so we can reduce our debugging turn-around time. Not necessary
    for returning the right status code of a failed job, but will be very
    helpful for post-mortem if the job fails.
    
    Signed-off-by: Taihsiang Ho (tai271828) <taihsiang.ho@canonical.com>
    Acked-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_nvidia_server_driver/nvidia-fs/a-c-t-entry.sh
+++ ubuntu_nvidia_server_driver/nvidia-fs/a-c-t-entry.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
+# make sure a-c-t invoke the script in the right directory context
 run_test() {
     exe_dir=$(dirname "${BASH_SOURCE[0]}")
     pushd "${exe_dir}"

2e63f8e1a6e29eae3e90f4b2374be4509f3028ec
Po-Hsu Lin <po-hsu.lin@canonical.com>
2022-08-08 14:12:06 +0800

    UBUNTU: SAUCE: ubuntu_ltp_cve: split test out of ubuntu_ltp
    BugLink: https://bugs.launchpad.net/bugs/1983666
    
    Some of the CVE tests will check the kernel taint flag to determine if
    the test pass or not.
    
    So if some other test failed before the CVE subset, like the
    read_all_sys in the fs subset (bug 1973153), it will cause
    false-positive failures.
    
    It can also reduce the ubuntu_ltp execution time, avoid test being
    killed by the 4hrs jenkins timeout threshold. Another benefit to pull
    this out is that we can have more flexibility to select what we want
    to test.
    
    The downside is the time cost for one extra deployment for this job.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

---
+++ ubuntu_ltp_cve/ubuntu_ltp_cve.py
@@ -0,0 +1,135 @@
+#
+#
+import multiprocessing
+import os
+import platform
+import re
+import shutil
+from autotest.client                        import test, utils
+
+class ubuntu_ltp_cve(test.test):
+    version = 1
+
+    def install_required_pkgs(self):
+        try:
+            series = platform.dist()[2]
+        except AttributeError:
+            import distro
+            series = distro.codename()
+
+        pkgs = [
+            'automake',
+            'bison',
+            'build-essential',
+            'byacc',
+            'flex',
+            'git',
+            'keyutils',
+            'libacl1-dev',
+            'libaio-dev',
+            'libcap-dev',
+            'libmm-dev',
+            'libnuma-dev',
+            'libsctp-dev',
+            'libselinux1-dev',
+            'libssl-dev',
+            'libtirpc-dev',
+            'pkg-config',
+            'quota',
+            'xfslibs-dev',
+            'xfsprogs',
+        ]
+        gcc = 'gcc' if self.arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        if any(x in self.flavour for x in ['aws', 'azure', 'gcp', 'gke']):
+            pkgs.append('linux-modules-extra-' + platform.uname()[2])
+
+        cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
+    def initialize(self):
+        self.flavour = re.split('-\d*-', platform.uname()[2])[-1]
+        self.arch = platform.processor()
+
+    # setup
+    #
+    #    Automatically run when there is no autotest/client/tmp/<test-suite> directory
+    #
+    def setup(self):
+        self.install_required_pkgs()
+        self.job.require_gcc()
+        os.chdir(self.srcdir)
+        shutil.rmtree('ltp', ignore_errors=True)
+        branch = 'sru'
+        cmd = 'git clone -b {} git://git.launchpad.net/~canonical-kernel-team/+git/ltp'.format(branch)
+        utils.system_output(cmd, retain_output=True)
+
+        # Print test suite HEAD SHA1 commit id for future reference
+        os.chdir(os.path.join(self.srcdir, 'ltp'))
+        title_local = utils.system_output("git log --oneline -1 | sed 's/(.*)//'", retain_output=False, verbose=False)
+        title_upstream = utils.system_output("git log --oneline | grep -v SAUCE | head -1", retain_output=False, verbose=False)
+        print("Latest commit in '{}' branch: {}".format(branch, title_local))
+        print("Latest upstream commit: {}".format(title_upstream))
+
+        # Disable NTFS as we disable RW support
+        cmd = 'sed -i /ntfs/d lib/tst_supported_fs_types.c'
+        utils.system_output(cmd)
+
+        utils.make('autotools')
+        utils.configure()
+        try:
+            nprocs = '-j' + str(multiprocessing.cpu_count())
+        except:
+            nprocs = ''
+        utils.make(nprocs)
+        utils.make('install')
+
+
+    # run_once
+    #
+    #    Driven by the control file for each individual test.
+    #
+    def run_once(self, test_name):
+        if test_name == 'setup':
+            return
+
+        # PHLin - LTP 20220527, most of the default timeout is now just 30 seconds instead of 5 mins, mulitplier must be modified
+        LTP_TIMEOUT_MUL = '50'
+
+        if test_name == 'zram01':
+            print("Setting LTP_TIMEOUT_MUL={} for zram01 test (lp:1897556)".format(LTP_TIMEOUT_MUL))
+            os.environ["LTP_TIMEOUT_MUL"] = LTP_TIMEOUT_MUL
+        elif test_name in ['cve-2018-1000204', 'ioctl_sg01']:
+            print("Setting LTP_TIMEOUT_MUL={} for cve-2018-1000204 / ioctl_sg01 (lp:1899413, lp:1936886, lp:1949934, lp:1955629)".format(LTP_TIMEOUT_MUL))
+            os.environ["LTP_TIMEOUT_MUL"] = LTP_TIMEOUT_MUL
+        elif test_name == 'cve-2017-5754':
+            print("Setting LTP_TIMEOUT_MUL={} for cve-2017-5754 (lp:1981780)".format(LTP_TIMEOUT_MUL))
+            os.environ["LTP_TIMEOUT_MUL"] = LTP_TIMEOUT_MUL
+        elif test_name in ['cve-2017-15299', 'cve-2017-15951']:
+            print("Setting LTP_TIMEOUT_MUL={} for cve-2017-15299 / cve-2017-15951 (lp:1983299)")
+            os.environ["LTP_TIMEOUT_MUL"] = LTP_TIMEOUT_MUL
+        elif test_name == 'fs_fill':
+            print("Setting LTP_TIMEOUT_MUL={} for fs_fill".format(LTP_TIMEOUT_MUL))
+            os.environ["LTP_TIMEOUT_MUL"] = LTP_TIMEOUT_MUL
+        elif test_name == 'read_all_proc':
+            print("Setting LTP_TIMEOUT_MUL={} for read_all_proc".format(LTP_TIMEOUT_MUL))
+            os.environ["LTP_TIMEOUT_MUL"] = LTP_TIMEOUT_MUL
+        elif test_name == 'read_all_sys':
+            print("Setting LTP_TIMEOUT_MUL={} for read_all_sys".format(LTP_TIMEOUT_MUL))
+            os.environ["LTP_TIMEOUT_MUL"] = LTP_TIMEOUT_MUL
+
+        cmd = '/opt/ltp/runltp -f /tmp/target -q -C /dev/null -l /dev/null -T /dev/null'
+        print(utils.system_output(cmd, verbose=False))
+        # /dev/loop# creation will be taken care by the runltp
+
+    def cleanup(self, test_name):
+        if test_name == 'setup':
+            return
+
+        # Restore the timeout multiplier
+        if 'LTP_TIMEOUT_MUL' in os.environ:
+            print("Restore timeout multiplier LTP_TIMEOUT_MUL back to default")
+            del os.environ["LTP_TIMEOUT_MUL"]
+
+# vi:set ts=4 sw=4 expandtab syntax=python:

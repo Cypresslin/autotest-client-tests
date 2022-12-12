a448c2271046fa38e2466dad5d8805cba2c19767
Po-Hsu Lin <po-hsu.lin@canonical.com>
2022-12-12 13:02:38 +0800

    UBUNTU: SAUCE: ubuntu_sysdig_smoke_test: fix build issue on Kinetic+
    BugLink: https://bugs.launchpad.net/bugs/1998612
    
    The sysdig-dkms package has been replaced with falcosecurity-scap-dkm
    since Kinetic+.
    
    Update the package list accordingly to fix this issue.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Francis Ginther <francis.ginther@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_sysdig_smoke_test/ubuntu_sysdig_smoke_test.py
+++ ubuntu_sysdig_smoke_test/ubuntu_sysdig_smoke_test.py
@@ -1,5 +1,6 @@
 #
 #
+import platform
 from autotest.client                        import test, utils
 from autotest.client.shared                 import error
 
@@ -8,19 +9,35 @@ class ubuntu_sysdig_smoke_test(test.test):
 
     def install_required_pkgs(self):
         pkgs = [
-            'sysdig', 'sysdig-dkms'
+            'sysdig'
         ]
+        pkgs.append(self.dkms_pkg)
 
         cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
         self.results = utils.system_output(cmd, retain_output=True, ignore_status=True)
 
     def initialize(self):
+        try:
+            self.series = platform.dist()[2]
+        except AttributeError:
+            import distro
+            self.series = distro.codename()
+
+        dkms = 'scap'
+        self.dkms_pkg = 'falcosecurity-scap-dkms'
+        self.cleanup_target = 'scap'
+        log_path = '/var/lib/dkms/{}/kernel-*/log/make.log'.format(dkms)
+        if self.series in ['xenial', 'bionic', 'focal', 'jammy']:
+            dkms = 'sysdig'
+            self.dkms_pkg = 'sysdig-dkms'
+            self.cleanup_target = 'sysdig_probe'
+            log_path = '/var/lib/dkms/{}/*/build/make.log'.format(dkms)
         self.install_required_pkgs()
-        cmd = 'dkms status -m sysdig | grep installed'
+        cmd = 'dkms status -m {} | grep installed'.format(dkms)
         try:
             utils.system(cmd)
         except error.CmdError:
-            cmd = 'cat /var/lib/dkms/sysdig/*/build/make.log'
+            cmd = 'cat {}'.format(log_path)
             utils.system(cmd)
             raise error.TestError('DKMS failed to install')
 
@@ -30,9 +47,9 @@ class ubuntu_sysdig_smoke_test(test.test):
         print(self.results)
 
     def cleanup(self):
-        cmd = 'modprobe -r sysdig_probe || true'
+        cmd = 'modprobe -r {} || true'.format(self.cleanup_target)
         self.results = utils.system_output(cmd, retain_output=False)
-        cmd = 'apt-get remove --purge sysdig-dkms -y'
+        cmd = 'apt-get remove --purge {} -y'.format(self.dkms_pkg)
         self.results = utils.system_output(cmd, retain_output=False)
 
 

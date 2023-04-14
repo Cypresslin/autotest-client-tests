a894d2067ca0b8e388406a89f569b974610b82d5
Po-Hsu Lin <po-hsu.lin@canonical.com>
2023-04-14 17:11:57 +0800

    UBUNTU: SAUCE: Install modules-extra package for IBM kernel
    BugLink: https://bugs.launchpad.net/bugs/2016239
    
    With B-ibm-5.4, the following tests would require modules from the
    modules-extra package:
      * ubuntu_zram_smoke_test
      * ubuntu_quota_smoke_test
      * ubuntu_kernel_selftests/net:rtnetlink.sh (LP: #1913678)
    
    We also install the modules-extra package for ubuntu_ltp* with some
    cloud kernels, but after examining the test result, I don't think we
    need it for B-ibm-5.4. They will be omitted to save us some time.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
+++ ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -42,7 +42,7 @@ class ubuntu_kernel_selftests(test.test):
         gcc = 'gcc' if self.arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
         pkgs.append(gcc)
 
-        if any(x in self.flavour for x in ['aws', 'azure', 'gcp', 'gke', 'oracle']):
+        if any(x in self.flavour for x in ['aws', 'azure', 'gcp', 'gke', 'ibm', 'oracle']):
             if not (self.flavour == 'aws' and self.series == 'trusty'):
                 pkgs.append('linux-modules-extra-' + platform.uname()[2])
 

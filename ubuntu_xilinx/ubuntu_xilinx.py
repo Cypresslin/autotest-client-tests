b6ff29c526f66aa35ca78f7cff5e7c9c73e22fa4
Po-Hsu Lin <po-hsu.lin@canonical.com>
2023-05-25 15:01:02 +0800

    UBUNTU: SAUCE: ubuntu_xilinx: fix test for Jammy
    On the re-provisioned KV260 with Jammy Xilinx kernel, this test will
    complain about:
     1. Missing xlnx-config command, this can be fix by installing the
        xlnx-config snap. I guess we don't have this issue on F-xilinx
        because this snap has already been included in the image.
     2. Mismatched name in /proc/device-tree/compatible, we are looking
        for 'k26' here, it works for Focal but not Jammy. We will need
        'kv26' here for Jammy
        $ cat /proc/device-tree/compatible
        xlnx,zynqmp-sk-kv260-rev1xlnx,zynqmp-sk-kv260-revBxlnx,zynqmp-sk-kv260xlnx,zynqmp
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Francis Ginther <francis.ginther@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_xilinx/ubuntu_xilinx.py
+++ ubuntu_xilinx/ubuntu_xilinx.py
@@ -7,6 +7,11 @@ class ubuntu_xilinx(test.test):
     def initialize(self):
         pass
 
+    def setup(self):
+        snap_pkgs = ['xlnx-config']
+        cmd = 'snap install --classic ' + ' '.join(snap_pkgs)
+        utils.system_output(cmd, retain_output=True)
+
     def run_once(self, test_name):
         cmd = os.path.join(self.bindir, 'tests', test_name)
         utils.system_output(cmd, retain_output=True)

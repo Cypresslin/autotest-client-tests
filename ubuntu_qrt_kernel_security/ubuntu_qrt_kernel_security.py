cbbb34bc6578ccd41e2aac8252926255699f07cb
Po-Hsu Lin <po-hsu.lin@canonical.com>
2023-06-21 18:08:27 +0800

    UBUNTU: SAUCE: ubuntu_qrt_kernel_security: install gcc-12 on Jammy non 5.15
    On J-hwe-6.2, test_072_strict_devmem and test_120_smep_works are
    failing with gcc-12 not found issue as well. Instead of keep adding
    regex rules, I think we can just install gcc-12 for all non-5.15
    kernels on Jammy.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_qrt_kernel_security/ubuntu_qrt_kernel_security.py
+++ ubuntu_qrt_kernel_security/ubuntu_qrt_kernel_security.py
@@ -15,9 +15,8 @@ class ubuntu_qrt_kernel_security(test.test):
         ]
         gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
         pkgs.append(gcc)
-        # Special case for J-oem-6.1 (LP: #2009782)
         if self.series == 'jammy':
-            if re.match('6\.1\.0-.*oem', platform.release()) or re.match('5\.19\.0-.*' ,platform.release()):
+            if not re.match('5\.15\.0-.*', platform.release()):
                 pkgs.append('gcc-12')
 
         cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)

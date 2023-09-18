Cypresslin/autotest-client-tests

commit c81876bff7e81b5b983664669a3f5416f0657167

UBUNTU: SAUCE: ubuntu_kernel_selftests: remove package requirement constraint for libnuma-dev and libfuse-dev
BugLink: https://bugs.launchpad.net/bugs/2033520

The build for net tests on X-hwe-s390x will fail with:
  reuseport_bpf_numa.c:24:18: fatal error: numa.h: No such file or directory

Also reuseport_bpf_numa, reuseport_dualstack and reuseaddr_conflict tests
will fail to start because ther are not executable.

Add libnuma-dev and libfuse-dev directly to pkgs, we used to limit these
two for non T/X-s390x as they were not built for s390x back then.

These two packages are available for s390x now, and since we don't run
ubuntu_kernel_selftests on Trusty the if statement can be removed.

$ rmadison -a s390x libfuse-dev
 libfuse-dev | 2.9.4-1ubuntu3   | xenial          | s390x
 libfuse-dev | 2.9.4-1ubuntu3.1 | xenial-updates  | s390x
$ rmadison -a s390x libnuma-dev
 libnuma-dev | 2.0.11-1ubuntu1.1   | xenial-security | s390x
 libnuma-dev | 2.0.11-1ubuntu1.1   | xenial-updates  | s390x

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
Acked-by: Sean Feole <sean.feole@canonical.com>
Acked-by: Francis Ginther <francis.ginther@canonical.com>
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -22,6 +22,8 @@ class ubuntu_kernel_selftests(test.test):
             'iptables',
             'jq',
             'kernel-wedge',
+            'libfuse-dev'
+            'libnuma-dev',
             'libssl-dev',
             'netsniff-ng',
             'net-tools',
@@ -30,9 +32,6 @@ class ubuntu_kernel_selftests(test.test):
         ]
         if not self.series in ['trusty', 'xenial', 'bionic']:
             pkgs.append('socat')
-        if not (self.arch == 's390x' and self.series in ['trusty', 'xenial']):
-            pkgs.append('libnuma-dev')
-            pkgs.append('libfuse-dev')
         if not self.arch == 's390x':
             if not self.series in ['trusty', 'xenial', 'bionic', 'focal', 'jammy']:
                 # With recent kernels BPF requires lld (LLVM-based linker) to

commit 5e9182c5933bce94135f4752011ab7ad02bae097

UBUNTU: SAUCE: ubuntu_kernel_selftests: fix missing delimiter for packages
There ought to be a comma after libfuse-dev package.

Fixes: c81876bff7 ("UBUNTU: SAUCE: ubuntu_kernel_selftests: remove
package requirement constraint for libnuma-dev and libfuse-dev")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -22,7 +22,7 @@ class ubuntu_kernel_selftests(test.test):
             'iptables',
             'jq',
             'kernel-wedge',
-            'libfuse-dev'
+            'libfuse-dev',
             'libnuma-dev',
             'libssl-dev',
             'netsniff-ng',
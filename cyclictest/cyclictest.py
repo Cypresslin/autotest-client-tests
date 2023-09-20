c6a762e6fbbb5d288768fad93a9cf83b03a174d8
Po-Hsu Lin <po-hsu.lin@canonical.com>
2023-09-20 12:21:46 +0800

    UBUNTU: SAUCE: cyclictest: install gcc for the test and add a setup task
    BugLink: https://bugs.launchpad.net/bugs/2036687
    
    This test will need gcc to run. Also add a setup task to separate
    test build phase and actual test phase.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Sean Feole <sean.feole@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- cyclictest/cyclictest.py
+++ cyclictest/cyclictest.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from autotest.client import test
 from autotest.client.shared import utils
 
@@ -11,10 +12,30 @@ class cyclictest(test.test):
     def initialize(self):
         pass
 
+    def install_required_pkgs(self):
+        arch   = platform.processor()
+        try:
+            series = platform.dist()[2]
+        except AttributeError:
+            import distro
+            series = distro.codename()
+
+        pkgs = [
+            'build-essential',
+        ]
+        gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
     def setup(self):
+        self.install_required_pkgs()
         self.job.require_gcc()
         os.chdir(self.srcdir)
         utils.make()
 
-    def execute(self, args='-t 10 -l 100000'):
+    def run_once(self, test_name, args='-t 10 -l 100000'):
+        if test_name == 'setup':
+            return
         utils.system(self.srcdir + '/cyclictest ' + args)

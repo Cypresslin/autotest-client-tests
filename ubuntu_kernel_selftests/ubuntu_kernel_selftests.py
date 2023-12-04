4e558ca80bd20b92d80a10d01f8e8b34dc519796
Po-Hsu Lin <po-hsu.lin@canonical.com>
2023-12-04 20:40:28 +0800

    UBUNTU: SAUCE: ubuntu_kernel_selftests: clean net test related code
    net test has been moved into ubuntu_kselftests_net, related code
    can be removed now.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Sean Feole <sean.feole@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
+++ ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -12,54 +12,16 @@ class ubuntu_kernel_selftests(test.test):
     def install_required_pkgs(self):
         '''Function to install necessary packages.'''
         pkgs = [
-            'bc',
+            'bc',           # For memory-hotplug
             'build-essential',
-            'devscripts',
-            'docutils-common',
-            'ethtool',
-            'fuse',
-            'git',
-            'iptables',
-            'jq',
-            'kernel-wedge',
-            'libfuse-dev',
-            'libnuma-dev',
-            'libssl-dev',
-            'netsniff-ng',
-            'net-tools',
+            'fuse',         # For memfd
+            'kernel-wedge', # For "fakeroot debian/rules clean"
+            'libcap-dev',   # For seccomp
+            'libfuse-dev',  # For memfd
             'pkg-config',
-            'uuid-runtime'
         ]
-        if not self.series in ['trusty', 'xenial', 'bionic']:
-            pkgs.append('socat')
-        if not self.arch == 's390x':
-            if not self.series in ['trusty', 'xenial', 'bionic', 'focal', 'jammy']:
-                # With recent kernels BPF requires lld (LLVM-based linker) to
-                # build the corresponding kernel selftests, so make sure this
-                # package is installed (in the releases where it is available)
-                # lld is not available for s390x
-                pkgs.append('lld')
         gcc = 'gcc' if self.arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
         pkgs.append(gcc)
-
-        if any(x in self.flavour for x in ['aws', 'azure', 'gcp', 'gke', 'ibm', 'oracle']):
-            if not (self.flavour == 'aws' and self.series == 'trusty'):
-                pkgs.append('linux-modules-extra-' + platform.uname()[2])
-
-        if self.kv >= 415:
-            # extra packages for building bpf tests
-            pkgs.extend(['libcap-dev', 'libelf-dev'])
-            if self.kv in [504, 503]:
-                # special case for B-5.4 (lp:1882559) / B-5.3 (lp:1845860)
-                # clang on F is clang-10 but we need clang-9 (see commit 95f91d59642)
-                # clang on E is clang-9, so it's ok to just check kv here
-                pkgs.extend(['clang-9', 'llvm-9'])
-            elif self.kv == 506:
-                # special case for F-oem-5.6 (lp:1879360)
-                pkgs.extend(['clang-10', 'llvm-10'])
-            else:
-                pkgs.extend(['clang', 'llvm'])
-
         cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
         utils.system_output(cmd, retain_output=True)
 
@@ -71,8 +33,6 @@ class ubuntu_kernel_selftests(test.test):
         except AttributeError:
             import distro
             self.series = distro.codename()
-        self.kv = platform.release().split(".")[:2]
-        self.kv = int(self.kv[0]) * 100 + int(self.kv[1])
 
     def download(self):
         '''Function to download kernel source.'''
@@ -192,13 +152,6 @@ class ubuntu_kernel_selftests(test.test):
                     cmd = 'sed -i "s/ mem-on-off-test.sh//" ' + mk
                     utils.system(cmd)
 
-            # net/txtimestamp.sh is very fragile, disable it
-            #
-            fn = 'linux/tools/testing/selftests/net/Makefile'
-            if os.path.exists(fn):
-                cmd = 'sed -i "/^TEST_PROGS += txtimestamp.sh$/d" ' + fn
-                utils.system(cmd)
-
             # Build header first (LP: #2031400)
             if not self.series in ['trusty', 'xenial', 'bionic', 'focal']:
                 cmd = "make -C linux/ headers"
@@ -210,32 +163,6 @@ class ubuntu_kernel_selftests(test.test):
             return
         if test_name.endswith('-build'):
             os.chdir(self.srcdir)
-            if "net" in test_name:
-                cmds = []
-                cmds.append("sh -c 'echo 1 > /proc/sys/net/ipv4/conf/all/accept_local'")
-                # The net benchmarching tests (e.g. udpgso) can fail when
-                # optmem limit is reached.
-                # https://www.kernel.org/doc/html/latest/networking/msg_zerocopy.html#transmission
-                # LP #1960907
-                cmds.append("sh -c 'echo 2048000 > /proc/sys/net/core/optmem_max'")
-                for cmd in cmds:
-                    utils.system(cmd)
-
-                if self.kv >= 415:
-                    # net selftests use a module built by bpf selftests, bpf is available since bionic kernel
-                    if self.kv == 506:
-                        os.environ["CLANG"] = "clang-10"
-                        os.environ["LLC"] = "llc-10"
-                        os.environ["LLVM_OBJCOPY"] = "llvm-objcopy-10"
-                        os.environ["LLVM_READELF"] = "llvm-readelf-10"
-                    elif self.kv in [504, 503]:
-                        os.environ["CLANG"] = "clang-9"
-                        os.environ["LLC"] = "llc-9"
-                        os.environ["LLVM_OBJCOPY"] = "llvm-objcopy-9"
-                        os.environ["LLVM_READELF"] = "llvm-readelf-9"
-                    cmd = "make -C linux/tools/testing/selftests TARGETS=bpf SKIP_TARGETS= KDIR=/usr/src/linux-headers-{}".format(platform.release())
-                    # keep running selftests/net, even if selftests/bpf build fails
-                    utils.system(cmd, ignore_status=True)
             cmd = "make -C linux/tools/testing/selftests TARGETS={}".format(test_name.replace('-build', ''))
             utils.system_output(cmd, retain_output=True)
             return
@@ -246,17 +173,6 @@ class ubuntu_kernel_selftests(test.test):
         os.chdir(dir_root)
         cmd = "make run_tests -C {} TEST_PROGS={} TEST_GEN_PROGS='' TEST_CUSTOM_PROGS=''".format(category, sub_test)
         result = utils.system_output(cmd, retain_output=True)
-
-        # The output of test_bpf.sh / test_blackhole_dev.sh test will be in the dmesg
-        kernel_module_tests = {'test_bpf.sh': 'CONFIG_TEST_BPF',
-                               'test_blackhole_dev.sh': 'CONFIG_TEST_BLACKHOLE_DEV'}
-        if sub_test in kernel_module_tests.keys():
-            output = utils.system_output('dmesg', retain_output=True)
-            if not output:
-                print("Looks like there's no dmesg output, checking for {}...".format(kernel_module_tests[sub_test]))
-                cmd = "grep ^{} /boot/config-$(uname -r)".format(kernel_module_tests[sub_test])
-                if not utils.system_output(cmd, verbose=False, ignore_status=True):
-                    print("{} not enabled.".format(kernel_module_tests[sub_test]))
 
         # Old pattern for Xenial
         pattern = re.compile('selftests: *(?P<case>[\w\-\.]+) \[FAIL\]\n')

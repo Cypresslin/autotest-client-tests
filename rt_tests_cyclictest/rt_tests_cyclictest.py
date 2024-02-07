0ca0197fa9f5eaf18fd4d41a90d7ecf524290069
Kevin Becker <kevin.becker@canonical.com>
2024-02-07 14:36:28 -0500

    UBUNTU: SAUCE: Add cyclictest from upstream rt-tests with pass/fail.
    Signed-off-by: Kevin Becker <kevin.becker@canonical.com>

---
+++ rt_tests_cyclictest/rt_tests_cyclictest.py
@@ -0,0 +1,79 @@
+import multiprocessing
+import os
+import platform
+import re
+import shutil
+from autotest.client import test, utils
+from autotest.client.shared import error
+
+class rt_tests_cyclictest(test.test):
+    version = 1
+
+    def initialize(self):
+        self.flavour = re.split('-\d*-', platform.uname()[2])[-1]
+        self.arch = platform.processor()
+
+    def install_required_pkgs(self):
+        try:
+            series = platform.dist()[2]
+        except AttributeError:
+            import distro
+            series = distro.codename()
+
+        pkgs = [
+            'build-essential',
+            'git',
+            'libnuma-dev',
+        ]
+        gcc = 'gcc' if self.arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
+    # setup
+    #
+    #    Automatically run when there is no autotest/client/tmp/<test-suite> directory
+    #
+    def setup(self):
+        self.install_required_pkgs()
+        self.job.require_gcc()
+        os.chdir(self.srcdir)
+        shutil.rmtree('rt-tests', ignore_errors=True)
+        branch = 'main'
+        cmd = 'git clone -b {} https://git.kernel.org/pub/scm/utils/rt-tests/rt-tests.git'.format(branch)
+        utils.system_output(cmd, retain_output=True)
+
+        # Print test suite HEAD SHA1 commit id for future reference
+        os.chdir(os.path.join(self.srcdir, 'rt-tests'))
+        title_local = utils.system_output("git log --oneline -1 | sed 's/(.*)//'", retain_output=False, verbose=False)
+        title_upstream = utils.system_output("git log --oneline | grep -v SAUCE | head -1", retain_output=False, verbose=False)
+        print("Latest commit in '{}' branch: {}".format(branch, title_local))
+        print("Latest upstream commit: {}".format(title_upstream))
+
+        try:
+            nprocs = '-j' + str(multiprocessing.cpu_count())
+        except:
+            nprocs = ''
+        utils.make(nprocs)
+
+
+    # run_once
+    #
+    #    Driven by the control file for each individual test.
+    #
+    #    Runs cyclictest with 10 threads, for 100000 loops, priority set to 
+    #    80 and an interval of 200us. It will fail if the max latency goes 
+    #    over 100us.
+    #
+    def run_once(self, test_name, args='-t 10 -m -l 100000 -p 80 -b 100 -i 200 -d 0', exit_on_error=True):
+        if test_name == 'setup':
+            return
+
+        self.results = utils.system_output(self.srcdir + '/rt-tests/cyclictest ' + args, retain_output=True)
+
+        if "Break" == self.results.splitlines()[-1].split()[1]:
+            raise error.TestError('FAIL: Max latency over 100us.')
+
+        return
+

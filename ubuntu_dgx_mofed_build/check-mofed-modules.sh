75589b87136307212feb7d65fade6f058598df95
Jacob Martin <jacob.martin@canonical.com>
2024-06-18 15:45:20 -0500

    UBUNTU: SAUCE: ubuntu_dgx_mofed_build: Allow expected module lists to target a specific major kernel version
    The MOFED DKMS package can build additional modules when certain kernel
    configs are enabled. For example, our 6.5 and 6.8 Nvidia kernels have
    CONFIG_MLX5_VFIO_PCI enabled, while 5.15 does not have the option at
    all. With it enabled, the MOFED DKMS packages build the
    "mlx5-vfio-pci.ko" kernel module, which causes the test to fail with our
    current list meant for K5.15.
    
    Resolve this by allowing expected module lists to be specified for a
    particular major kernel version, while falling back to the less specific
    list if a specific one doesn't exist.
    
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_dgx_mofed_build/check-mofed-modules.sh
+++ ubuntu_dgx_mofed_build/check-mofed-modules.sh
@@ -12,9 +12,19 @@ printf " %s\n" $release
 printf "INFO: Detecting MOFED driver version..."
 mofedver="$(dpkg-query --showformat='${Version}' --show mlnx-ofed-kernel-only)"
 printf " %s\n" ${mofedver}
-printf "INFO: Detecting Kernel version... %s\n" "$(uname -r)"
+printf "INFO: Detecting Kernel version..."
+kernelver="$(uname -r)"
+printf " %s\n" ${kernelver}
 actual="$(mktemp)"
-expected="$(pwd)/expected-mofed-modules/${mofedver}-${release}.lst"
+
+# First check for a list specific to this major kernel version
+expected="$(pwd)/expected-mofed-modules/${mofedver}-${release}-${kernelver%%-*}.lst"
+if [ ! -f "${expected}" ]; then
+	# Fall back to a general list for this MOFED version & series combination.
+	printf "INFO: No expected module list at '%s', falling back to " "${expected}"
+	expected="$(pwd)/expected-mofed-modules/${mofedver}-${release}.lst"
+	printf "'%s'\n" "${expected}"
+fi
 
 # This test assumes that the only modules installed here are the MOFED ones.
 # If other DKMS packages are installed that will throw it off.
@@ -25,6 +35,8 @@ if [ ! -f ${expected} ]; then
     echo "ERROR: No expected modules list available for MOFED $mofedver on $release" 1>&2
     exit 1
 fi
+
+echo "INFO: Using expected module list file '${expected}'"
 
 if diff -u ${expected} ${actual}; then
     echo "INFO: Success: Actual module list matches expected module list."

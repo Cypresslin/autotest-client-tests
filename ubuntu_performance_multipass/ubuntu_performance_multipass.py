Cypresslin/autotest-client-tests

commit 57acba3b4c9b80d0a4a18190a9e30a80fbe2bd79

UBUNTU: SAUCE: ubuntu_performance_multipass: allow retry if systemd-analyze is not ready
BugLink: https://bugs.launchpad.net/bugs/2071635

In some cases despite command "uname -r" is capable of getting the VM
kernel version, but "systemd-analyze" is not ready yet to get the
startup time.

Use a loop to check it 18 more times, with 10 seconds interval.

Output example:
Running 'multipass list'
 Name                    State             IPv4             Image
 test1bionic             Running           10.146.203.242   Ubuntu 18.04 LTS
Running 'multipass exec test1bionic -- uname -r'
 4.15.0-212-generic
Running 'multipass exec test1bionic -- systemd-analyze'
 Bootup is not yet finished. Please try again later.
 Wait for 10 seconds and try again.
Running 'multipass exec test1bionic -- systemd-analyze'
 Bootup is not yet finished. Please try again later.
 Wait for 10 seconds and try again.
Running 'multipass exec test1bionic -- systemd-analyze'
 Startup finished in 7.579s (kernel) + 54.559s (userspace) = 1min 2.139s

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
Acked-by: Kevin Becker <kevin.becker@canonical.com>
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_performance_multipass/ubuntu_performance_multipass.py
@@ -197,8 +197,27 @@ class ubuntu_performance_multipass(test.test):
         else:
             kernel = 'unknown'
 
-        results = self.multipass_run_cmd_output(vm, 'systemd-analyze')
-        stats = self.parse_systemd_analyze(results)
+        attempts = 18
+        interval = 10
+        retval = None
+        # Deal with cases where systemd-analyze is not ready yet (LP: #2071635)
+        while attempts >= 0:
+            attempts -= 1
+            try:
+                results = self.multipass_run_cmd_output(vm, 'systemd-analyze')
+                stats = self.parse_systemd_analyze(results)
+                retval = [ kernel, stats ]
+                break
+            except error.CmdError as err:
+                if 'Bootup is not yet finished' in err.result_obj.stderr:
+                    if attempts >= 0:
+                        print('Wait for {} seconds and try again.'.format(interval))
+                        time.sleep(interval)
+                    else:
+                        raise error.TestError('Failed to finish boot within {}*{} seconds'.format(attempt, interval))
+                else:
+                    raise error.TestError('Unknown command error: {}', err)
+                continue
 
         cmd = 'multipass delete ' + vm
         if self.multipass_run_cmd(cmd):
@@ -206,7 +225,7 @@ class ubuntu_performance_multipass(test.test):
         cmd = 'multipass purge'
         if self.multipass_run_cmd(cmd):
             print("failed to purge")
-        return [ kernel, stats ]
+        return retval
 
     def run_once(self, test_name):
         if test_name == 'setup':

commit 70ca12ce565f5e95f6ae54b30d8a13362a05bee6

UBUNTU: SAUCE: ubuntu_performance_multipass: fix missing import for error
Fix the "NameError: global name 'error' is not defined" issue.

Fixes: 57acba3b4 ("UBUNTU: SAUCE: ubuntu_performance_multipass: allow retry if systemd-analyze is not ready")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
Acked-by: Francis Ginther <francis.ginther@canonical.com>
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_performance_multipass/ubuntu_performance_multipass.py
@@ -3,6 +3,7 @@
 import os
 import re
 from autotest.client                        import test, utils
+from autotest.client.shared                 import error
 from math import sqrt
 import functools
 import platform
c938c57aa0c0bce094b7097a0983cee8fd4e4023
Po-Hsu Lin <po-hsu.lin@canonical.com>
2024-09-24 15:10:04 +0800

    UBUNTU: SAUCE: ubuntu_qrt_apparmor: add bubblewrap pacakge for Bionic+
    BugLink: https://bugs.launchpad.net/bugs/2081794
    
    Since commit dfdd4781b4 "Also test bwrap transitioning to unprivileged
    userns" in qa-regression-testing. test_unconfined_userns test in this
    test will require the bwrap command from bubblewrap package.
    
    This bubblewrap package is available for Bionic and newer.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Sean Feole <sean.feole@canonical.com>
    Signed-off-by: Sean Feole <sean.feole@canonical.com>

--- ubuntu_qrt_apparmor/ubuntu_qrt_apparmor.py
+++ ubuntu_qrt_apparmor/ubuntu_qrt_apparmor.py
@@ -65,6 +65,9 @@ class ubuntu_qrt_apparmor(test.test):
         if self.series not in ['trusty', 'xenial', 'bionic', 'focal', 'jammy', 'lunar']:
             pkgs.append('liburing-dev') # LP: #2044230
 
+        if self.series not in ['trusty', 'xenial']:
+            pkgs.append('bubblewrap') # LP: #2081794
+
         cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
         self.results = utils.system_output(cmd, retain_output=True)
 

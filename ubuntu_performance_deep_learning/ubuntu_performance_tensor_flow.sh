28b09e4ecc1c494a403f56b05f7b8fc0a837ba61
Jacob Martin <jacob.martin@canonical.com>
2024-11-22 11:52:06 -0600

    UBUNTU: SAUCE: ubuntu_performance_deep_learning: use nvidia-container-toolkit on Noble
    The nvidia-docker2 package has been deprecated and is not available for Noble.
    
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_performance_deep_learning/ubuntu_performance_tensor_flow.sh
+++ ubuntu_performance_deep_learning/ubuntu_performance_tensor_flow.sh
@@ -6,16 +6,37 @@
 set -eo pipefail
 
 CONTAINER_VER="23.03"
+DISTRIBUTION="$(. /etc/os-release;echo "$ID$VERSION_ID")"
 
-install_nvidia_docker() {
-    local distribution
-    distribution="$(. /etc/os-release;echo $ID$VERSION_ID)"
+install_nvidia_docker_legacy() {
     curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-    curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+    curl -s -L "https://nvidia.github.io/nvidia-docker/$DISTRIBUTION/nvidia-docker.list" | \
         sudo tee /etc/apt/sources.list.d/nvidia-docker.list > /dev/null
     sudo apt update
     sudo apt install nvidia-docker2 -y
     sudo systemctl restart docker
+}
+
+install_nvidia_docker_nct() {
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
+    gpg --dearmor -o - | sudo tee /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg && \
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sudo apt update
+    sudo apt install docker.io nvidia-container-toolkit -y
+    sudo nvidia-ctk runtime configure --runtime=docker
+    sudo systemctl restart docker
+}
+
+install_nvidia_docker() {
+    # nvidia-docker is deprecated and not available for Noble,
+    # so use the more modern nvidia-container-toolkit.
+    if [ "$DISTRIBUTION" = "ubuntu24.04" ]; then
+        install_nvidia_docker_nct
+    else
+        install_nvidia_docker_legacy
+    fi
 }
 
 get_num_gpus() {
@@ -28,7 +49,7 @@ setup() {
     install_nvidia_docker
 }
 
-run_test() {
+run_test_legacy() {
     sudo nvidia-docker run \
          --shm-size=1g \
          --ulimit memlock=-1 \
@@ -45,6 +66,34 @@ run_test() {
          --num_iter=300 \
          --iter_unit=batch \
          --display_every=300
+}
+
+run_test_nct() {
+    sudo docker run \
+         --gpus all \
+         --shm-size=1g \
+         --ulimit memlock=-1 \
+         --ulimit stack=67108864 \
+         --rm nvcr.io/nvidia/tensorflow:${CONTAINER_VER}-tf1-py3 -- \
+         mpiexec \
+         --bind-to socket \
+         --allow-run-as-root \
+         -np "$(get_num_gpus)" \
+         python -u /workspace/nvidia-examples/cnn/resnet.py \
+         --layers=50 \
+         --precision=fp16 \
+         --batch_size=256 \
+         --num_iter=300 \
+         --iter_unit=batch \
+         --display_every=300
+}
+
+run_test() {
+    if [ "$DISTRIBUTION" = "ubuntu24.04" ]; then
+        run_test_nct
+    else
+        run_test_legacy
+    fi
 }
 
 case $1 in

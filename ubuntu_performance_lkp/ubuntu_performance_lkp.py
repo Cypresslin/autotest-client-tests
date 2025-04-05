fc992bda2f9e41f8b6b802ae84dbd6903f0bb51a
Edoardo Canepa <edoardo.canepa@canonical.com>
2025-04-05 10:21:17 +0200

    UBUNTU: SAUCE: ubuntu_performance_lkp: avoid setting squid proxy if unreacheable
    BugLink: https://bugs.launchpad.net/bugs/2106287
    
    Even if a proxy is always needed, as is it not directly involved in the test case
    it is best to not set it if unreacheable
    
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>
    Acked-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_performance_lkp/ubuntu_performance_lkp.py
+++ ubuntu_performance_lkp/ubuntu_performance_lkp.py
@@ -11,6 +11,7 @@ import shutil
 import socket
 import subprocess
 import resource
+import requests
 
 #
 # Number of test iterations to get min/max/average stats
@@ -114,6 +115,19 @@ class ubuntu_performance_lkp(test.test):
         finally:
             s.close()
         return ipaddr
+    
+    def set_proxy(self):
+        try:
+            proxies = {
+                "http": "http://squid.internal:3128",
+                "https": "http://squid.internal:3128",
+            }
+            response = requests.get("https://github.com", proxies=proxies)
+            print(response.json())
+            os.environ["https_proxy"] = "http://squid.internal:3128"
+            os.environ["http_proxy"] = "http://squid.internal:3128"
+        except requests.exceptions.ProxyError as e:
+            print("Not using squid.internal proxy:", e)    
 
     def setup(self, lkp_jobs, lkp_commit):
         self.get_sysinfo()
@@ -121,8 +135,7 @@ class ubuntu_performance_lkp(test.test):
         self.job.require_gcc()
 
         if "192.168." not in self.get_ip():
-            os.environ["https_proxy"] = "http://squid.internal:3128"
-            os.environ["http_proxy"] = "http://squid.internal:3128"
+            self.set_proxy()
 
         os.chdir(self.srcdir)
         shutil.rmtree('lkp-tests', ignore_errors=True)

61f3a3a591464dfd0a47d914699a808f5fa9626d
Benjamin Wheeler <benjamin.wheeler@canonical.com>
2025-04-07 15:26:56 -0400

    UBUNTU: SAUCE: New rtla timerlat test that parses/prints results.
    Signed-off-by: Benjamin Wheeler <benjamin.wheeler@canonical.com>
    Acked-by: Kevin Becker <kevin.becker@canonical.com>
    Acked-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- rtla_timerlat/rtla_timerlat.py
+++ rtla_timerlat/rtla_timerlat.py
@@ -6,5 +6,5 @@ class rtla_timerlat(test):
 
     def run_once(self, test_name, args="", exit_on_error=True):
         timeout_s = 60 * 10
-        utils.system_output("{}/timerlat_test.sh {}".format(self.bindir, timeout_s), retain_output=True)
+        utils.system_output("{}/timerlat_test --duration {}".format(self.bindir, timeout_s), retain_output=True)
 
--- rtla_timerlat/timerlat_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Pass timeout in seconds as an argument
-timeout --preserve-status --signal INT $1 rtla timerlat hist
-
-# Forward the exit code
-exit $?

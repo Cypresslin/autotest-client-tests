31d18bee3d66d399e98a943781de6fefe55ede56
Abdur Rahman <abdur.rahman@canonical.com>
2025-07-30 14:44:57 -0400

    UBUNTU: SAUCE: ubuntu_nvidia_fs: fix bug in importing gpg key
    gpg uses --yes instead of -y. This is used to prevent prompt to
    overwrite gpg key file, in case this command was run before.
    
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>
    Acked-by: Sean Feole <sean.feole@canonical.com>
    Signed-off-by: Sean Feole <sean.feole@canonical.com>

--- ubuntu_nvidia_fs/nvidia-fs/test-nvidia-fs.sh
+++ ubuntu_nvidia_fs/nvidia-fs/test-nvidia-fs.sh
@@ -99,7 +99,7 @@ function setup_nvme {
 
 #Install Nvidia Container Toolkit
 function install_nvidia_ctk {
-        curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg -y --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+        curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
 && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
 sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
 sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
